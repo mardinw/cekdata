52e59c99d773b28ce7fe293366a3774fba04324c
mardinw <mardinw@outlook.com>
2024-11-19 00:37:02 +0000

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,12 @@ import { authMiddleware } from './middlewares/auth.js'
 const app = new Hono()
 
 // middleware
-app.use(cors());
+app.use('*',cors({
+    origin: 'http://103.217.145.5', // atau '*', jika ingin mengizinkan semua asal
+    allowMethods: ['GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+  }
+));
 app.use(secureHeaders());
 if(process.env.HONO_ENV !== 'production'){
   app.use(logger());
