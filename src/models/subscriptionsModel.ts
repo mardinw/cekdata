7cb9e62829ac55315277aeecd301ebc16b47ba92
mardinw <mardinw@outlook.com>
2024-11-21 15:06:39 +0700

    subscriptions support

---
+++ src/models/subscriptionsModel.ts
@@ -0,0 +1,36 @@
+import { db } from "../utils/db";
+
+export const insertSubscriptions = async(
+    uuid: string,
+    token: string,
+    limit_count: number,
+    is_active: number,
+    expireAt: number,
+) => {
+    const query = 'INSERT INTO subscriptions(users, token, limit_count, is_active, expire_at) VALUES(?, ?, ?, ?, ?)';
+    const [result]  =  await db.query(query, [uuid, token, limit_count, is_active, expireAt]);
+    return result;
+}
+
+export const setActiveSubscriptions = async( 
+    is_active: number,
+    uuid: string,
+) => {
+    const query = 'UPDATE subscriptions SET is_active = ? WHERE users = ?';
+    const [result] = await db.query(query, [is_active, uuid]);
+    return result;
+}
+
+export const getSubscriptionsByUUID = async(
+    uuid: number,
+) => {
+    const query = 'SELECT limit_count, token, is_active, created_at FROM subscriptions WHERE users = ?';
+    const [result] = await db.query(query, uuid);
+    return result;
+}
+
+export const getSubscriptionsByAdmin = async() => {
+    const query = 'SELECT t1.users as uuid, t2.users as users, t1.limit_count as limit_count, t1.token, t1.is_active, t1.created_at FROM subscriptions t1 INNER JOIN users t2 ON t2.id = t1.users GROUP BY t1.users';
+    const [result] = await db.query(query);
+    return result;
+}
